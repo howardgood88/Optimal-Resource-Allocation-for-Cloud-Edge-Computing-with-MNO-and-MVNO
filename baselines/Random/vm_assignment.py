Finished testing weighting factor. Fix vm price getting lower and lower.

baselines/Random/vm_assignment.py
@@ -11,7 +11,7 @@ class VMAssignment:
         self.optimizing = VMAssignmentOptimizing(contract, candidate_vm_id, vm_list)
         self.candidate_vm_id = candidate_vm_id
         self.vm_list = vm_list
-        self.vm_highest_price = sum([vm.price for vm in vm_list.values()])
+        self.vm_highest_price = sum([vm.origin_price for vm in vm_list.values()])
     
     def run(self, statistic_data: np.array) -> None:
         '''Start running VM Assignment algorithm.'''
@@ -26,7 +26,7 @@ class VMAssignment:
                     for vm_id in selected_vm_id:
                         # reverse by highest price value to make optimizing maximize other than minimize.
                         # _lambda is the discount MNO provide to MVNO
-                        cost += self.vm_list[vm_id].price * _lambda
+                        cost += self.vm_list[vm_id].origin_price * _lambda
                     fitness = self.vm_highest_price - cost
                     logging.debug(f'population {idx + 1} {selected_vm_id} with cost: {cost}, fitness: {fitness}')
                     self.optimizing.fitness[idx] = fitness

baselines/VM Load Balance/utils.py
@@ -60,14 +60,14 @@ class step_logger:
 
 def get_total_resource(vm_id_list: np.array, vm_list: dict):
     _sum = [[0, 0, 0] for _ in range(3)]
-    edge_num, cloud_num = 0, 0
+    edge_num, cloud_num = [0, 0, 0], [0, 0, 0]
     for vm_id in vm_id_list:
         vm = vm_list[vm_id]
-        if vm.location == 'cloud':
-            cloud_num += 1
-        else:
-            edge_num += 1
         task_type_idx = Task_type_index[vm.task_type].value
+        if vm.location == 'cloud':
+            cloud_num[task_type_idx] += 1
+        else:
+            edge_num[task_type_idx] += 1
         _sum[task_type_idx][0] += vm.cr
         _sum[task_type_idx][1] += vm.avg_bw_up
         _sum[task_type_idx][2] += vm.avg_bw_down

data/nonstationary_poisson_arrival.py
@@ -11,8 +11,6 @@ np.random.seed(rnd_seed)
 # hour_traffic_ratio = [0.51, 0.42, 0.33, 0.31, 0.23, 0.23, 0.24, 0.22, 0.24, 0.33, 0.35, 0.52, 0.56, 0.56, 0.64, 0.8, 0.91, 0.97, 0.98, 0.95, 0.92, 0.965, 0.87, 0.8]
 # hour 1~24
 day_hour_traffic_ratio = [0.09, 0.09, 0.07, 0.07, 0.07, 0.13, 0.44, 0.73, 0.77, 0.78, 0.72, 0.48, 0.69, 0.69, 0.47, 0.26, 0.20, 0.19, 0.12, 0.10, 0.08, 0.07, 0.07, 0.07]
-history_hour_traffic_ratio = [sum(day_hour_traffic_ratio) / len(day_hour_traffic_ratio) for _ in range(24)]
-# print(f'history ratio: {history_hour_traffic_ratio[0]}')
 
 user_num = 300
 machine_num = 300
@@ -43,17 +41,16 @@ def machine_generator(filename):
         with open(dir + filename, 'w') as f:
             f.write(_str)
 
-def task_events_generator(filename, hour_traffic_ratio):
-    def event_gen(_type, max_cpu, bw_up_attr, bw_down_attr):
-        cpu_req = np.random.random() * max_cpu
-        return [event_id, 0, t, _type, str(np.random.randint(0, user_num)), cpu_req, cpu_req * np.random.random(),
+def task_events_generator(filename, days, hour_traffic_ratio):
+    def event_gen(_type, cpu, bw_up_attr, bw_down_attr):
+        return [event_id, 0, t, _type, str(np.random.randint(0, user_num)), cpu, beta(4, 4, 0.01, cpu - 0.005),
                 beta(*bw_up_attr), beta(*bw_down_attr)]
     t = 0
     event_id = 0
     event_in = {}
     
     _str = '[\n'
-    for i in range(number_of_days):
+    for i in range(days):
         if i % 7 < 5:
             r = 1
         else:
@@ -114,6 +111,6 @@ if __name__ == '__main__':
         os.makedirs('./data')
     plot()
     machine_generator('machine_attributes.json')
-    task_events_generator('task_events.json', day_hour_traffic_ratio)
-    task_events_generator('history_data.json', history_hour_traffic_ratio)
+    task_events_generator('task_events.json', number_of_days, day_hour_traffic_ratio)
+    task_events_generator('history_data.json', 1, day_hour_traffic_ratio)
     print(f'Finished generating, save result to {out_files}')